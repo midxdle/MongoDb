4f4b9e7143fc349bafffe821ac67896a0456a48c
midxdle <sina.heydarian.sh@gmail.com>
2021-08-14 23:30:48 +0430

    now I could change or update data base with this file

---
+++ UpdateData.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");                                                                                                                                    
+const url = "mongodb+srv://midxdle:fFbE2DpWoxmGTAXF@cluster0.axsj3.mongodb.net";
+const client = new MongoClient(url);
+const dbName = "sample_mflix";
+async function run() {
+  try {
+       await client.connect();
+       console.log("Connected correctly to server");
+       const db = client.db(dbName);
+
+       // Use the collection ""
+       const col = db.collection("sessions");
+
+       const myDoc = await col.updateOne({
+           "user_id":"New_id"
+       },{
+           $set: {
+               "user_id": "xxx@yyy.com"
+           }
+       });
+
+       const myDoc = await col.findOne();
+         // Print to the console
+         console.log(myDoc);
+  }
+         finally {
+          await client.close();
+  }
+}
+
+run().catch(console.dir);
